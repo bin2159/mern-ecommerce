73c129f624f5322e420f21f738238979cf8576f8
bin2159 <bipinthms007@gmail.com>
2023-10-01 14:03:38 +0530

    deploy test 7

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ app.post('/webhook', express.raw({type: 'application/json'}), (request, response
 });
 
 
+app.use(express.static(path.resolve(__dirname,'build')))
 app.use(cookieParser())
 app.use(express.json())
 app.use(morgan('dev'))
@@ -78,17 +79,6 @@ app.use(
   })
 )
 
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-  app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
-          if(err) {
-              res.status(500).send(err)
-          }
-      });
-  })
-}
 
 
 app.use('/products', isAuth(), productRouter)
